0033e98b7dbddcbd6ba2e609c15151dea8548fa2
Helena Santoro <79323367+HelenaSantoro@users.noreply.github.com>
2024-05-30 20:06:24 +0200

    Create calculator.py
    Example of a calculator

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# Calculator in Python
+
+def add(a, b):
+  answer = a + b
+  return answer
+
+def subtract(a, b):
+  answer = a - b
+  return answer
+   
+
+def multiply(a, b):
+  answer = a * b
+  return answer
+
+def divide(a, b):
+  answer = a / b
+  return answer
+
+def  exp(a, b):
+  answer = a ** b
+  return answer 
+
+print(add(100, 6))
+print(exp(100, 6))
+print(subtract(-100, 6))
+print(divide(100, 6))
+print(multiply(100, -6))
